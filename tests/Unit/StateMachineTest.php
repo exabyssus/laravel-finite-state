709a060c82231d29a3ae64ecb2e76695bfc68baa
Edgars Skujins <eddyonboard@gmail.com>
2019-08-08 09:00:56 +0300

    Add more tests

--- tests/Unit/StateMachineTest.php
+++ tests/Unit/StateMachineTest.php
@@ -5,32 +5,164 @@ namespace Tests\Unit;
 use Exabyssus\StateMachine\StateMachine;
 use Exabyssus\StateMachine\Traits\HasStateHistory;
 use Exabyssus\StateMachine\Traits\HasStateMachine;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\File;
 use Tests\CreatesApplication;
 use Tests\TestCase;
+use Mockery;
 
+/**
+ * Class StateMachineTest
+ * @package Tests\Unit
+ */
 class StateMachineTest extends TestCase
 {
     use CreatesApplication;
 
     protected $stateMachine;
 
-    public function setUp()
+    /**
+     * @return void
+     */
+    public function setUp(): void
     {
         parent::setUp();
-        Config::set('state-machine.order', include( __DIR__.'/../../config/state-machine.php');
-        $this->stateMachine = new StateMachine(new Order());
+        Config::set('state-machine', include(__DIR__ . '/../../config/state-machine.php'));
     }
 
-    public function testCan(){
-        $this->stateMachine->can('confirm');
+    /**
+     * @return void
+     */
+    public function testCan(): void
+    {
+        $stateMachine = new StateMachine(new Order());
+        $can = $stateMachine->can('confirm');
+        $this->assertTrue($can);
     }
 
+    /**
+     * @return void
+     */
+    public function testGetState(): void
+    {
+        $stateMachine = new StateMachine(new Order());
+        $state = $stateMachine->getState();
+        $this->assertEquals('pending', $state);
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetPossibleTransitions(): void
+    {
+        $stateMachine = new StateMachine(new Order());
+        $possible = $stateMachine->getPossibleTransitions();
+        $this->assertIsArray($possible);
+        $possible = array_values($possible);
+        $this->assertEquals(['confirm', 'cancel'], $possible);
+    }
+
+    /**
+     * @return void
+     */
+    public function testTransitionAllowed(): void
+    {
+        $model = new Order();
+        $this->assertTrue($model->transitionAllowed('confirm'));
+        $this->assertFalse($model->transitionAllowed('complete'));
+    }
+
+    /**
+     * @return void
+     */
+    public function testApply(): void
+    {
+        $model = new Order();
+        $stateMachine = new StateMachine($model);
+        $stateMachine->apply('confirm');
+        $this->assertEquals('confirmed', $model->state);
+    }
+
+    /**
+     * @return void
+     */
+    public function testFindHistoricalStatesByTo(): void
+    {
+        $model = new Order();
+        $stateMachine = new StateMachine($model);
+        $stateMachine->apply('confirm');
+        $this->assertEquals('confirmed', $model->state);
+
+        $historyLine = $model->findHistoricalStatesByTo('confirmed')->first();
+
+        $this->assertEquals($historyLine->owner_type, Order::class);
+        $this->assertEquals(1, $historyLine->owner_id);
+        $this->assertEquals('confirm', $historyLine->transition);
+    }
+
+    /**
+     * @return void
+     */
+    public function testFindHistoricalStatesByTransition(): void
+    {
+        $model = new Order();
+        $stateMachine = new StateMachine($model);
+        $stateMachine->apply('confirm');
+        $this->assertEquals('confirmed', $model->state);
+
+        $historyLine = $model->findHistoricalStatesByTransition('confirm')->first();
+
+        $this->assertEquals($historyLine->owner_type, Order::class);
+        $this->assertEquals(1, $historyLine->owner_id);
+        $this->assertEquals('confirm', $historyLine->transition);
+    }
+
+    /**
+     * @return void
+     */
+    public function testHasStateInHistory(): void
+    {
+        $model = new Order();
+        $stateMachine = new StateMachine($model);
+        $stateMachine->apply('confirm');
+        $this->assertEquals('confirmed', $model->state);
+
+        $this->assertTrue($model->hasStateInHistory('confirmed'));
+        $this->assertFalse($model->hasStateInHistory('canceled'));
+    }
+
+    /**
+     * @return void
+     */
+    public function testHasTransitionInHistory(): void
+    {
+        $model = new Order();
+        $stateMachine = new StateMachine($model);
+        $stateMachine->apply('confirm');
+        $this->assertEquals('confirmed', $model->state);
+
+        $this->assertTrue($model->hasTransitionInHistory('confirm'));
+        $this->assertFalse($model->hasTransitionInHistory('cancel'));
+    }
 }
 
-class Order {
+class Order extends Model
+{
     use HasStateMachine, HasStateHistory;
 
+    /**
+     * Order constructor.
+     * @param string $state
+     */
+    public function __construct(string $state = 'pending')
+    {
+        $this->state = $state;
+        $this->id = 1;
+    }
 
+    public function save(array $options = [])
+    {
+        return true;
+    }
 }
